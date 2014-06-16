1d9962383390a970367bbe6fb0e46393f6c69e78
adrails <adrails123@gmail.com>
2014-06-16 00:32:40 -0700

    Added profile picture

--- app/models/profile_detail.rb
+++ app/models/profile_detail.rb
@@ -1,12 +1,14 @@
 class ProfileDetail < ActiveRecord::Base
 	# Setup accessible (or protected) attributes for your model
-  attr_accessible :contact_no, :gender, :dob, :doj, :user_id
+  attr_accessible :contact_no, :gender, :dob, :doj, :user_id, :avatar
 	
 	#Associations
 	belongs_to :user
 
  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   #validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+validates_attachment_content_type :avatar, :content_type => %w(image/jpeg image/jpg)
 
 	has_many :addresses, dependent: :destroy
 	accepts_nested_attributes_for :addresses
