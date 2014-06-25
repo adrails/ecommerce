a5256ee80b3fb3062817dfed949069ac9242cb15
adrails <adrails123@gmail.com>
2014-06-25 02:40:35 -0700

    slideer moved to new file

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,9 @@ ECommerce::Application.routes.draw do
   devise_for :users
 	devise_scope :user do 
 		get "sign_in", :to => "devise/sessions#new" 
-		root :to => 'devise/sessions#new'
+		#~ root :to => 'devise/sessions#new'
 	end
+	root :to => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
