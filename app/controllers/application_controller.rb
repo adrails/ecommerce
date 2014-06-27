5b470a42704029bac9d31a2626f4cc0d6247d283
adrails <adrails123@gmail.com>
2014-06-27 05:54:22 -0700

    spelled correct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 	before_filter :update_sanitized_params, if: :devise_controller?
-	before_filer :create_cart
+	before_filter :create_cart
 	
 	rescue_from ActiveRecord::RecordNotFound, :with => :not_found
 	rescue_from ActionController::RoutingError, :with => :rescue_404
